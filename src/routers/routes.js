e06f3aa35dca7ddba528a4368390bd0b2923c67e
letsovew <ovewknds@gmail.com>
2024-08-15 15:48:51 +0900

    데이터베이스 모델 설정

---
+++ src/routers/routes.js
@@ -0,0 +1,21 @@
+//AUTH
+const JOIN = "/auth/join"
+const LOGIN = "/auth/login"
+const AUTH_REGISTER = "/auth/register"
+
+//USER
+const USER_UPDATE = "/users/update/:id"
+const USER_SCHEDULE_REQ = "/users/schedules/request"
+
+//PARTNER
+const PARTNER_SCHEDULES = "/partners/schedules"
+
+
+//POST
+const LIST = "/posts/list"
+const CREATE = "/posts/create"
+
+//PET
+const PET_REGISTER = "/pets/register"
+
+//SCHEDULE
